7b51651010fa50087f21a1c289f07e6ceef33f83
CodePhilanthropist <barrigaete@gmail.com>
2023-03-16 10:29:20 +0800

    add almost finished

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -47,21 +47,87 @@ class LoginScreen extends StatelessWidget {
                       ),
                     ],
                   ),
+                  Padding(
+                    padding: const EdgeInsets.only(bottom: 30),
+                    child: Row(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: const [
+                        Padding(
+                          padding: EdgeInsets.only(right: 16),
+                          child: Icon(
+                            Icons.alternate_email,
+                            color: kPrimaryColor,
+                          ),
+                        ),
+                        Expanded(
+                          child: TextField(
+                            decoration: InputDecoration(hintText: "Email"),
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
                   Row(
                     crossAxisAlignment: CrossAxisAlignment.center,
                     children: const [
                       Padding(
                         padding: EdgeInsets.only(right: 16),
                         child: Icon(
-                          Icons.alternate_email,
+                          Icons.lock,
                           color: kPrimaryColor,
                         ),
                       ),
                       Expanded(
                         child: TextField(
-                          decoration: InputDecoration(hintText: "Email"),
+                          decoration: InputDecoration(hintText: "Password"),
                         ),
                       )
+                    ],
+                  ),
+                  Row(
+                    children: [
+                      Container(
+                        padding: EdgeInsets.all(16),
+                        decoration: BoxDecoration(
+                          shape: BoxShape.circle,
+                          border: Border.all(
+                            color: Colors.white.withOpacity(.5),
+                          ),
+                        ),
+                        child: Icon(
+                          Icons.android,
+                          color: Colors.white.withOpacity(.5),
+                        ),
+                      ),
+                      const SizedBox(
+                        width: 16,
+                      ),
+                      Container(
+                        padding: EdgeInsets.all(16),
+                        decoration: BoxDecoration(
+                          shape: BoxShape.circle,
+                          border: Border.all(
+                            color: Colors.white.withOpacity(.5),
+                          ),
+                        ),
+                        child: Icon(
+                          Icons.chat,
+                          color: Colors.white.withOpacity(.5),
+                        ),
+                      ),
+                      const Spacer(),
+                      Container(
+                        padding: EdgeInsets.all(16),
+                        decoration: BoxDecoration(
+                          color: kPrimaryColor,
+                          shape: BoxShape.circle,
+                          border: Border.all(),
+                        ),
+                        child: const Icon(
+                          Icons.arrow_forward,
+                          color: Colors.black,
+                        ),
+                      ),
                     ],
                   )
                 ],
